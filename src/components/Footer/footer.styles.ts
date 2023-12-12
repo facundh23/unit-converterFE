d448ece0607fcf16a165e31e6dbf0b3dbf49ea5b
Facundo Herrera <facundhfed@gmail.com>
2023-12-12 11:23:19 +0100

    Fonts checked

--- src/components/Footer/footer.styles.ts
+++ src/components/Footer/footer.styles.ts
@@ -5,9 +5,8 @@ export const StyledFooter = styled.footer`
   background-color: #2e0039;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   width: 100%;
-  gap: 10px;
 `;
 export const StyledFooterItems = styled.p`
   color: white;
